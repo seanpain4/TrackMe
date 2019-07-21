c086b1cb3badfbbb8c6af20e2f3dce5f71d39632
seanpain4 <seanpain4@gmail.com>
2019-07-21 12:12:41 +1000

    FIX: Fix untracked files

---
+++ web/public/app.js
@@ -0,0 +1,26 @@
+$('#navbar').load('navbar.html');
+$('#footer').load('footer.html');
+
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function(device) {
+  $('#devices tbody').append(`
+    <tr>
+      <td>${device.user}</td>
+      <td>${device.name}</td>
+    </tr>`
+  );
+});
+
+$('#add-device').on('click', function() {
+  const user = $('#user').val();
+  const name = $('#name').val();
+  devices.push({ user: user, name: name });
+  localStorage.setItem('devices', JSON.stringify(devices));
+  window.location.href = '/';
+});
+
+$('#send-command').on('click', function() {
+  const command = $('#command').val();
+  console.log(`command is: ${command}`);
+});
